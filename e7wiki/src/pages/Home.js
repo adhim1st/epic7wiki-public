d8235594d7d09f45963d89e61b5d39cb47791b6f
adhim1st <adhim@student.uns.ac.id>
2021-01-08 08:54:34 +0700

    fix demo link

--- e7wiki/src/pages/Home.js
+++ e7wiki/src/pages/Home.js
@@ -3,16 +3,17 @@ import FavouriteHeroCard from "../components/FavouriteHeroCard";
 export default function Home() {
   const favourite = useSelector((state) => state.favouriteReducer.favourite);
 
+  const favHeroList = favourite.length ? (
+    favourite.map((hero) => {
+      return <FavouriteHeroCard key={hero.id} hero={hero} />;
+    })
+  ) : (
+    <h3 className="center text-white"> No Favourite Hero Yet </h3>
+  );
   return (
-    <>
-      <div className="container">
-        <h1 style={{ color: "#9f9f9c" }}>Favourite Hero</h1>
-        <div className="row">
-          {favourite.map((hero) => {
-            return <FavouriteHeroCard key={hero.id} hero={hero} />;
-          })}
-        </div>
-      </div>
-    </>
+    <div className="container">
+      <h1 style={{ color: "#9f9f9c" }}>Favourite Hero</h1>
+      <div className="row">{favHeroList}</div>
+    </div>
   );
 }
